92b5965cf323948a2c86037abc921e1b8ee030ee
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-10 07:27:31 +1030

    init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+type Metadata = {};
+
+function getV1ProjectMetadataCid(projectId: number) {
+  return "";
+}
+
+function getV2ProjectMetadataCid(projectId: number) {
+  return "";
+}
+
+function getLogoCid(metadata: Metadata) {
+  return "";
+}
+
+function pin() {}
+
+function run() {}
